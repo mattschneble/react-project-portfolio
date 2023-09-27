a58cb15b1e5d71529a1b0a876ab8d0469c4fb97c
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-09-27 16:18:27 -0700

    updated footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,1 +1,10 @@
-s
+// create and export footer component
+
+export default function Footer() {
+    return (
+        <footer className="d-flex justify-content-center">
+            <a className="col-3 link-dark" href="https://github.com/mattschneble">Visit my GitHub!</a>
+            <a className="col-3 link-dark" href="https://www.linkedin.com/in/matthew-schneble-6a0a3b1b/">Visit my LinkedIn!</a>
+        </footer>
+    );
+}
