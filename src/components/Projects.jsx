7bddb92077b2147e5cab32b3dd9560737b853de3
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-09-27 18:04:28 -0700

    finished portfolio redesign

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,10 @@
-// create project component
+// create projects component
 
 const Projects = ({title, image, link, repo}) => {
     return(
         <div>
             <div className="card col-3">
-            <img src={image} class="card-img-top" style={{"height" : "75%", "width" : "75%"}}/>
+            <img src={image} className="card-img-top" style={{"height" : "75%", "width" : "75%"}}/>
             <a className="btn btn-primary" href={link} target="_blank">{title} Visit the Site!</a>
             <a className="btn btn-primary" href={repo} target="_blank">{title} GitHub Repo</a>
             </div>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,92 +1,26 @@
-// import screenshots for later use
-import JavaScriptQuiz from "../../public/JavaScriptQuiz.png";
-import TextEditor from "../../public/TextEditor.png";
-import PasswordGenerator from "../../public/PasswordGenerator.png";
-import FlavorByte from "../../public/FlavorByte.png";
-import ReelReviews from "../../public/ReelReviews.png";
-import HaikuHelper from "../../public/HaikuHelper.png";
+// import required components and modules
+import Project from "../components/Projects"
+import projects from "../utils/projectinfo.json"
 
-// create and export portfolio page
+// create and export Portfolio page
 export default function Portfolio() {
     return (
         <div>
-            <ul className="">
-                <div className="row1">
-
-                    <li className="col p-4 m-4">
-                        <div className="d-flex justify-content-center">
-                            <button className="m-2">
-                                <a href="https://haiku-helper-428f5c7f1218.herokuapp.com/">Haiku Helper</a>
-                            </button>
-                            <button className="m-2">
-                                <a href="https://github.com/ssherp/haiku-creator">GitHub Repo</a>
-                            </button>
-                        </div>
-                        <img src={HaikuHelper} alt="Haiku Helper" className="img-fluid" width={250}/>
-                    </li>
-
-                        <li className="col p-4 m-4">
-                        <div className="d-flex justify-content-center">
-                            <button className="m-2">
-                                <a href="https://cinemaserver-7dafae9cd971.herokuapp.com/">ReelReviews</a>
-                            </button>
-                            <button className="m-2">
-                                <a href="https://github.com/mattschneble/ReelReviews">GitHub Repo</a>
-                            </button>
-                        </div>
-                        <img src={ReelReviews} alt="ReelReviews" className="img-fluid" width={250}/>
-                    </li>
-
-                        <li className="col p-4 m-4">
-                        <div className="d-flex justify-content-center">
-                            <button className="m-2">
-                                <a href="https://rossfahan.github.io/flavorByte/">FlavorByte</a>
-                            </button>
-                            <button className="m-2">
-                                <a href="https://github.com/RossFahan/flavorByte">GitHub Repo</a>
-                            </button>
-                        </div>
-                        <img src={FlavorByte} alt="FlavorByte" className="img-fluid" width={250}/>
-                    </li>
-
-                        <li className="col p-4 m-4">
-                        <div className="d-flex justify-content-center">
-                            <button className="m-2">
-                                <a href="https://mattschneble.github.io/custom-password-generator/">Password Generator</a>
-                            </button>
-                            <button className="m-2">
-                                <a href="https://github.com/mattschneble/custom-password-generator">GitHub Repo</a>
-                            </button>
-                        </div>
-                        <img src={PasswordGenerator} alt="Password Generator" className="img-fluid" width={250}/>
-                    </li>
-
-                        <li className="col p-4 m-4">
-                        <div className="d-flex justify-content-center">
-                            <button className="m-2">
-                                <a href="https://fathomless-brushlands-17288-cc4c35c614a6.herokuapp.com/">Text Editor</a>
-                            </button>
-                            <button className="m-2">
-                                <a href="https://github.com/mattschneble/progressive-text-editor">GitHub Repo</a>
-                            </button>
-                        </div>
-                        <img src={TextEditor} alt="Text Editor" className="img-fluid" width={250}/>
-                    </li>
-
-                        <li className="col p-4 m-4">
-                        <div className="d-flex justify-content-center">
-                            <button className="m-2">
-                                <a href="https://mattschneble.github.io/knowledge-quiz/">JavaScript Quiz</a>
-                            </button>
-                            <button className="m-2">
-                                <a href="https://github.com/mattschneble/knowledge-quiz">GitHub Repo</a>
-                            </button>
-                        </div>
-                        <img src={JavaScriptQuiz} alt="JavaScript Quiz" className="img-fluid" width={250}/>
-
-                    </li>
+            <h1 className="text-center">Portfolio</h1>
+            <div className="container">
+                <div className="row">
+                    {projects.map((project) => (
+                        <Project
+                            key={project.id}
+                            name={project.name}
+                            image={project.image}
+                            description={project.description}
+                            github={project.github}
+                            deployed={project.deployed}
+                        />
+                    ))}
                 </div>
-            </ul>
+            </div>
         </div>
-    );
+    )
 }
