faea7a8f17c42bd6ec0c1cbc01e98c5fef24fbb3
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-09-27 17:02:28 -0700

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+// import all required modules, packages, utilities and components
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AboutMe from './pages/AboutMe.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Contact from './pages/Contact.jsx';
+import Resume from './pages/Resume.jsx';
+import Error from './pages/Error.jsx';
 import App from './App.jsx'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// create and export the router
+const router = createBrowserRouter([
+  // define routes
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
+      {
+        path: '/resume',
+        element: <Resume />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
 )
